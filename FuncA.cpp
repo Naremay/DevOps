8036286519eadf64af8b6613b7893fb732bdf7d7
Petrenko Alina <petrrenkoo@gmail.com>
2024-12-29 15:30:18 +0200

    Initial commit: Added FuncA class for hyperbolic series calculation.

---
+++ FuncA.cpp
@@ -0,0 +1,18 @@
+#include "FuncA.h"
+#include <cmath>
+
+/**
+ * @brief Calculate the sum of the first n elements of the hyperbolic cosine series.
+ * 
+ * @param x The input value for which the series is calculated.
+ * @param n The number of terms to include in the series sum.
+ * @return double The sum of the first n terms of the series.
+ */
+double FuncA::calculate(double x, int n) {
+    double sum = 0.0;
+    for (int k = 0; k < n; ++k) {
+        double term = std::pow(x, 2 * k) / tgamma(2 * k + 1);
+        sum += term;
+    }
+    return sum;
+}
---
+++ FuncA.h
@@ -0,0 +1,19 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+
+/**
+ * @class FuncA
+ * @brief Class to calculate the sum of n terms in the hyperbolic cosine series.
+ */
+class FuncA {
+public:
+    /**
+     * @brief Calculate the sum of n terms in the series.
+     * @param x Input value for calculation.
+     * @param n Number of terms to sum.
+     * @return Result of the series calculation.
+     */
+    double calculate(double x, int n);
+};
+
+#endif // FUNCA_H
